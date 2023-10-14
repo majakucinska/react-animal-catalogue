496801af09cf8df06fdc2ac894ede0886f262c98
Maja Kucinska <maja.kucinska@gmail.com>
2023-10-14 21:24:09 +0200

    Added h2 to app.js and made the description content more readable

---
+++ src/AfghanDesc.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AfghanDesc(){
+    return(
+        <p>
+            "The Afghan is one of the oldest breeds, getting his start thousands of years ago in a remote region of what is now Afghanistan, Pakistan, and India.
+            Originally used as a hunting dog and status symbol, the breed earned its keep with ease and is still prized today for his speed and agility. This prey drive is deeply ingrained in him and other sighthounds, making off-leash romps in open areas too dangerous for the breed. He’s also not recommended for life with cats or other small pets, either.
+            The Afghan gets a bad rep for scoring low on dog intelligence tests, but the breed is far from dumb. Instead, he’s one of the most independent canines, preferring to do things his way. Training an Afghan hound takes a lot of patience and creativity, but in the end, you’re rewarded with a highly affectionate, loyal companion."
+        </p>
+    )
+}
